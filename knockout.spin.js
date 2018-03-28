ba7497edb2499f113c035eba0c3ef6ae0512eb87
Zak B. Elep <zakame@zakame.net>
2018-03-28 10:11:30 +0800

    Use Promise instead of jQuery.Deferred
    Replace jQuery.Deferred with a Promise implementation, using either an
    available global from the browser, or a polyfill provided by pinkie.

--- knockout.spin.js
+++ knockout.spin.js
@@ -1,31 +1,31 @@
 var ko = require("knockout");
 var $ = require("jquery");
+var Promise = require("pinkie-promise");
 var Spinner = require("spin.js");
 
 ko.bindingHandlers.spinner = {
 	init: function (element, valueAccessor, allBindings) {
 		// initialize the object which will return the eventual spinner
-		var deferred = $.Deferred();
-		element.spinner = deferred.promise();
+		element.spinner = new Promise(function (resolve) {
+			// force this to the bottom of the event queue in the rendering thread,
+			// so we can get the computed color of the containing element after other bindings
+			// (e.g. class, style) have evalutated.
+			// HACK: add some more delay as the class bindings of the parent fire asynchronously.
+			setTimeout(function () {
+				var options = {};
+				options.color = $(element).css("color");
+				$.extend(options, ko.bindingHandlers.spinner.defaultOptions, ko.unwrap(allBindings.get("spinnerOptions")));
 
-		// force this to the bottom of the event queue in the rendering thread,
-		// so we can get the computed color of the containing element after other bindings
-		// (e.g. class, style) have evalutated.
-		// HACK: add some more delay as the class bindings of the parent fire asynchronously.
-		setTimeout(function () {
-			var options = {};
-			options.color = $(element).css("color");
-			$.extend(options, ko.bindingHandlers.spinner.defaultOptions, ko.unwrap(allBindings.get("spinnerOptions")));
-
-			deferred.resolve(new Spinner(options));
-		}, 30);
+				resolve(new Spinner(options));
+			}, 30);
+		});
 	},
 	update: function (element, valueAccessor) {
 		// when the spinner exists, pick up the existing one and call appropriate methods on it
 
 		var result = ko.unwrap(valueAccessor());
 
-		element.spinner.done(function (spinner) {
+		element.spinner.then(function (spinner) {
 			var isSpinning = result;
 			if (isSpinning) {
 				$(element).show();
